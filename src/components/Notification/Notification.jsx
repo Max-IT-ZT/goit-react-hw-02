f657e3284598b2f7b159e9c34a6eaceda4c1258f
Max-IT-ZT <babuskomax@gmail.com>
2024-06-18 01:56:34 +0300

    fix css

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,7 +1,9 @@
 import css from "./Notification.module.css";
+import { VscFeedback } from "react-icons/vsc";
 export default function Notification() {
   return (
     <div className={css.container}>
+      <VscFeedback size={50} />
       <p>No feedback yet</p>
     </div>
   );
